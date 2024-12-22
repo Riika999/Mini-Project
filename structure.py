712b3cd92fcca8c2ff6d9db5a101c22c28249c45
MARUTI-CODE <91046790+MARUTI-CODE@users.noreply.github.com>
2024-12-22 20:35:19 +0530

    Create structure.py

---
+++ structure.py
@@ -0,0 +1,11 @@
+backend/
+    ├── app/
+    │   ├── __init__.py 
+    │   ├── models.py 
+    │   ├── routes.py 
+    │   ├── services.py 
+    │   └── utils.py 
+    ├── requirements.txt
+    ├── Dockerfile (optional)
+    └── instance/
+        └── config.py
